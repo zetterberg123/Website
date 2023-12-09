ca000465f5cbd78a71e49544acbf83e0b9cad9e4
zetterberg123 <63219005+zetterberg123@users.noreply.github.com>
2023-12-09 19:20:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.zetterberg123.com/about",
+	site: "https://www.zetterberg123.com",
 });
